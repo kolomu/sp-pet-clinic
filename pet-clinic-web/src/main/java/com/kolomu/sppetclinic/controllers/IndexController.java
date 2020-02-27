e37bf217e2152fa000a8cbc558954076dd3a8869
kolomu <kevin-olomu@web.de>
2020-02-27 07:16:34 +0100

    Adding Index Page and controller. Closes #8

---
+++ pet-clinic-web/src/main/java/com/kolomu/sppetclinic/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package com.kolomu.sppetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    // give RequestMapping a list of parameters
+    @RequestMapping({"", "/", "index.html"})
+    public String index() {
+        return "index";
+    }
+}
