8e5e44ad875fa14b1ff99b16f7839875eafc4d55
kolomu <kevin-olomu@web.de>
2020-02-29 19:15:05 +0100

    Adding owners list and updated controller. Closes #16

--- pet-clinic-web/src/main/java/com/kolomu/sppetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/kolomu/sppetclinic/bootstrap/DataLoader.java
@@ -30,7 +30,7 @@ public class DataLoader implements CommandLineRunner {
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(1L);
+        owner2.setId(2L);
         owner2.setFirstName("Susan");
         owner2.setLastName("Sonnenschein");
         ownerService.save(owner2);
