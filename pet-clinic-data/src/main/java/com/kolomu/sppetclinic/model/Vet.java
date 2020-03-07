a4f4f05a2653590893b397a7bf290e44cad78238
kolomu <kevin-olomu@web.de>
2020-03-07 07:45:34 +0100

    added specialities, added to vet. Closes #27

--- pet-clinic-data/src/main/java/com/kolomu/sppetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/kolomu/sppetclinic/model/Vet.java
@@ -1,5 +1,16 @@
 package com.kolomu.sppetclinic.model;
 
+import java.util.Set;
+
 // Vet = Tierarzt
 public class Vet extends Person {
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
