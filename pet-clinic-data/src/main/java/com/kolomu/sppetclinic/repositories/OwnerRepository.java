68af7c6e96237f00610833ece6436b46de409952
kolomu <kevin-olomu@web.de>
2020-03-10 06:33:36 +0100

    added spring data jpa service for owners. Closes #40

--- pet-clinic-data/src/main/java/com/kolomu/sppetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/kolomu/sppetclinic/repositories/OwnerRepository.java
@@ -4,4 +4,6 @@ import com.kolomu.sppetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+    // using dynamic query method for spring data jpa
+    Owner findByLastName(String lastName);
 }
