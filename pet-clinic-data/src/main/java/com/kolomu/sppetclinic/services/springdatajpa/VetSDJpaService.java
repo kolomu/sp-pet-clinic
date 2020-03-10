c679fe8f0f3f8cd74f52052d9512fbce507990ec
kolomu <kevin-olomu@web.de>
2020-03-10 06:38:47 +0100

    added spring data jpa service for vets. Closes #41

---
+++ pet-clinic-data/src/main/java/com/kolomu/sppetclinic/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,49 @@
+package com.kolomu.sppetclinic.services.springdatajpa;
+
+import com.kolomu.sppetclinic.model.Vet;
+import com.kolomu.sppetclinic.repositories.VetRepository;
+import com.kolomu.sppetclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VetSDJpaService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetSDJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long aLong) {
+        return vetRepository.findById(aLong).orElse(null);
+    }
+
+    // maybe problem if we have unsaved specialties in here...
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        vetRepository.deleteById(aLong);
+    }
+}
